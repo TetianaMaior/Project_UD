bd9868ca9c394310e705a2525c1d8d0db69a06c6
TetianaMaior <Solomaho@ukr.net>
2025-01-04 10:42:13 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// window.alert("Hello")
+// const hello = confirm("Are you here?");
+
+// const answer = prompt("How old are you?", "20");
+// console.log(typeof(answer));
+
+// const arrUser = [];
+// arrUser[0] = prompt('Name');
+// arrUser[1] = prompt('Sername');
+// arrUser[2] = prompt('Age');
+
+// document.write(arrUser);
+
+
+const name = 'Tania';
+
+alert(`Hello, ${name}`);
